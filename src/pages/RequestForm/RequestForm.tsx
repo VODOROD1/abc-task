adec15ea7804d842cf0f4e29662f00b29bd586d1
Mikhail Beliakov <mr.mikhail-beliakov@yandex.ru>
2023-02-13 23:04:40 +0400

    Redux-form was used in requestForm component

---
+++ src/pages/RequestForm/RequestForm.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Form, Field } from 'react-final-form';
+
+const RequestReduxForm = reduxForm({ form: "request" })((props) => {
+  return (
+    <Form
+      initialValues={{
+        firstName: 'Dan'
+      }}
+      onSubmit={values => {
+        // send values to the cloud
+      }}
+      // validate={values => {
+      //   // do validation here, and return errors object
+      // }}
+    >
+      {() => (
+        <form onSubmit={props.handleSubmit}>
+          <Field component="input" type="text" name="request-number" />
+          <Field component="input" type="text" name="request-status" />
+          <Field component="input" type="text" name="client-name" />
+          <Field component="input" type="text" name="client-phonenumber" />
+          <Field component="input" type="text" name="manager-name" />
+          <Field component="input" type="text" name="date" />
+          <Field component="input" type="text" name="request-description" />
+          <button>Создать</button>
+        </form>
+      )}
+    </Form>
+  );
+});
+
+export default RequestForm;
