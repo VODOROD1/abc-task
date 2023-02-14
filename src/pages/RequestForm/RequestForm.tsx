f01b283e2f88a975313afca5c869f57119e597e0
Mikhail Beliakov <mr.mikhail-beliakov@yandex.ru>
2023-02-14 20:37:56 +0400

    react-final-form library was engaged

--- src/pages/RequestForm/RequestForm.tsx
+++ src/pages/RequestForm/RequestForm.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-// import { Form, Field } from "react-final-form";
-import { Field } from "redux-form";
+import { Form, Field } from "react-final-form";
+// import { Field } from "redux-form";
 import styles from "./RequestForm.module.scss";
 import { reduxForm } from "redux-form";
 
@@ -14,85 +14,101 @@ import { reduxForm } from "redux-form";
 //   requestDescription
 // }
 
+const requestForm = (props: any) => {
+    const onSubmit = (e: any) => {
+        debugger;
+
+    }
+
+    const validate = (e: any) => {
+        debugger;
+
+    }
+    debugger;
+    return (
+        <Form
+        initialValues={{
+            firstName: "Dan",
+        }}
+        // @ts-ignore
+        onSubmit={onSubmit}
+        // @ts-ignore
+        validate={validate}
+        render={({ handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+            <table>
+                <tbody>
+                <tr>
+                    <td className={styles.fieldName}>Номер заявки</td>
+                    <td>
+                    <Field
+                        component="input"
+                        type="text"
+                        name="requestNumber"
+                        placeholder="asfasdgsda"
+                    />
+                    </td>
+                </tr>
+                <tr>
+                    <td className={styles.fieldName}>Статус заявки</td>
+                    <td>
+                    <Field component="input" type="text" name="requestStatus" />
+                    </td>
+                </tr>
+                <tr>
+                    <td className={styles.fieldName}>Имя клиента</td>
+                    <td>
+                    <Field component="input" type="text" name="clientName" />
+                    </td>
+                </tr>
+                <tr>
+                    <td className={styles.fieldName}>Телефон клиента</td>
+                    <td>
+                    <Field
+                        component="input"
+                        type="text"
+                        name="clientPhonenumber"
+                    />
+                    </td>
+                </tr>
+                <tr>
+                    <td className={styles.fieldName}>Имя менеджера</td>
+                    <td>
+                    <Field component="input" type="text" name="managerName" />
+                    </td>
+                </tr>
+                <tr>
+                    <td className={styles.fieldName}>
+                    Дата и время создания/изменения статуса
+                    </td>
+                    <td>
+                    <Field component="input" type="text" name="date" />
+                    </td>
+                </tr>
+                <tr>
+                    <td className={styles.fieldName}>Описание заявки</td>
+                    <td>
+                    <Field
+                        component="input"
+                        type="text"
+                        name="requestDescription"
+                    />
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            <button className={styles.createButton} type={"submit"}>
+                Создать
+            </button>
+            </form>
+        )}
+        />
+    )
+}
+
 const RequestReduxForm = reduxForm({
   form: "request",
   enableReinitialize: true,
-})((props) => {
-  debugger;
-  return (
-    // <Form
-    //   initialValues={{
-    //     firstName: "Dan",
-    //   }}
-    //   onSubmit={(values) => {
-    //     // send values to the cloud
-    //     debugger;
-    //   }}
-    // validate={values => {
-    //   // do validation here, and return errors object
-    // }}
-    // >
-    // {() => (
-    // @ts-ignore
-    <form onSubmit={props.handleSubmit}>
-      <table>
-        <tbody>
-          <tr>
-            <td className={styles.fieldName}>Номер заявки</td>
-            <td>
-              <Field
-                component="input"
-                type="text"
-                name="requestNumber"
-                placeholder="asfasdgsda"
-              />
-            </td>
-          </tr>
-          <tr>
-            <td className={styles.fieldName}>Статус заявки</td>
-            <td>
-              <Field component="input" type="text" name="requestStatus" />
-            </td>
-          </tr>
-          <tr>
-            <td className={styles.fieldName}>Имя клиента</td>
-            <td>
-              <Field component="input" type="text" name="clientName" />
-            </td>
-          </tr>
-          <tr>
-            <td className={styles.fieldName}>Телефон клиента</td>
-            <td>
-              <Field component="input" type="text" name="clientPhonenumber" />
-            </td>
-          </tr>
-          <tr>
-            <td className={styles.fieldName}>Имя менеджера</td>
-            <td>
-              <Field component="input" type="text" name="managerName" />
-            </td>
-          </tr>
-          <tr>
-            <td className={styles.fieldName}>
-              Дата и время создания/изменения статуса
-            </td>
-            <td>
-              <Field component="input" type="text" name="date" />
-            </td>
-          </tr>
-          <tr>
-            <td className={styles.fieldName}>Описание заявки</td>
-            <td>
-              <Field component="input" type="text" name="requestDescription" />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <button className={styles.createButton} type={"submit"}>Создать</button>
-    </form>
-    //   )}
-    // </Form>
-  );
-});
+})(requestForm);
 
 export default RequestReduxForm;
